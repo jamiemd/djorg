b20daa261523eb22721080c52a9eff07a26ace84
Jamie Domingo <jaybmw@aol.com>
2018-04-16 13:40:21 -0700

    added authentication backends

--- djorg/settings.py
+++ djorg/settings.py
@@ -113,6 +113,10 @@ else:
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
+AUTHENTICATION_BACKENDS = (
+    ('django.contrib.auth.backends.ModelBackend'),
+)
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
