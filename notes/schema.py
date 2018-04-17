f081184f5e83d911c077a56cd78cdc204ac27565
Jamie Domingo <jaybmw@aol.com>
2018-04-17 10:45:48 -0700

    added notes schema

---
+++ notes/schema.py
@@ -0,0 +1,23 @@
+from graphene_django import DjangoObjectType
+import graphene
+from .models import Note as NoteModel
+
+class Note(DjangoObjectType):
+    # Transform data to Graphene respresentation
+    class Meta:
+        model = NoteModel
+        interfaces = (graphene.relay.Node, )
+
+class Query(graphene.ObjectType):
+    # Expose data results
+    notes = graphene.List(Note)
+
+    def resolve_notes(self, info):
+        import pdb; pdb.set_trace()
+        user = info.context.user
+        if user.is_anonymous:
+            return NoteModel.objects.none()
+        else: 
+            return NoteModel.objects.filter(user=user)
+
+schema = graphene.Schema(query=Query)
