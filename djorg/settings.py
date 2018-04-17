488c28c580c15b5047d722275458da4c29c1261a
Jamie Domingo <jaybmw@aol.com>
2018-04-16 21:08:27 -0700

    re-commented part of debug databases

--- djorg/settings.py
+++ djorg/settings.py
@@ -95,13 +95,13 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # }
 
 if DEBUG:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-# else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
     DATABASES = {
         'default': dj_database_url.config(
             default=config('DATABASE_URL')
