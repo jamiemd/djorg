6808faa19448b3e292532852a478580ebf491568
Jamie Domingo <jaybmw@aol.com>
2018-04-24 19:05:57 -0700

    working state

--- wiki/views.py
+++ wiki/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-# from .forms import WikiForm 
+from .forms import WikiForm 
 from .models import Wiki 
 
 def index(request): 
-    # if request.method == 'POST': 
-    #     form = WikiForm(request.POST) 
-    #     if form.is_valid(): 
-    #         form.save() 
-    # context = {'wiki': Wiki.objects.all(), 
-    #         'form': WikiForm()} 
-    context = {'wiki': Wiki.objects.all()} 
+    if request.method == 'POST': 
+        form = WikiForm(request.POST) 
+        if form.is_valid(): 
+            form.save() 
+    context = {'wikis': Wiki.objects.all(), 
+            'form': WikiForm()} 
     return render(request, 'wikis/index.html', context) 
 
